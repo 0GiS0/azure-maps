edb21fb59555634e4745ba68c833edb383093643
Gisela Torres Buitrago <giselat@microsoft.com>
2018-08-09 10:56:06 +0200

    Route API and Traffic API

---
+++ js/azure-maps-route.js
@@ -0,0 +1,84 @@
+map.addEventListener("click", searchLayer, (e) => {
+
+    var
+        startPoint = new atlas.data.Point([yourLocation.coords.longitude, yourLocation.coords.latitude]),
+        coords = JSON.parse("[" + e.features[0].properties.position + "]"),
+        destinationPoint = new atlas.data.Point([coords[1], coords[0]]),
+        startPin = new atlas.data.Feature(startPoint, {
+            title: "Your location",
+            icon: "pin-round-blue"
+        }),
+        destinationPin = new atlas.data.Feature(destinationPoint, {
+            title: e.features[0].properties.name,
+            icon: "pin-blue"
+        });
+
+    // Fit the map window to the bounding box defined by the start and destination points
+    var swLon = Math.min(startPoint.coordinates[0], destinationPoint.coordinates[0]),
+        swLat = Math.min(startPoint.coordinates[1], destinationPoint.coordinates[1]),
+        neLon = Math.max(startPoint.coordinates[0], destinationPoint.coordinates[0]),
+        neLat = Math.max(startPoint.coordinates[1], destinationPoint.coordinates[1]);
+
+    map.setCameraBounds({
+        bounds: [swLon, swLat, neLon, neLat],
+        padding: 50
+    });
+
+    // Add pins to the map for the start and end point of the route
+    map.addPins([startPin, destinationPin], {
+        name: "route-pins",
+        textFont: "SegoeUi-Regular",
+        textOffset: [0, -20]
+    });
+
+    //Get directions
+    var routeLayer = "routes";
+    map.addLinestrings([], {
+        name: routeLayer,
+        color: "#2272B9",
+        width: 5,
+        cap: "round",
+        join: "round",
+        before: "labels"
+    });
+
+    //Get the directions calling /route/directions
+    $.ajax({
+        url: 'https://atlas.microsoft.com/route/directions/json',
+        type: 'GET',
+        data: {
+            'api-version': '1.0',
+            'subscription-key': AzureMapsKey,
+            'query': startPoint.coordinates[1] + ',' + startPoint.coordinates[0] + ':' + destinationPoint.coordinates[1] + ',' + destinationPoint.coordinates[0]
+        }
+    }).done(function (result) {
+        var route = result.routes[0];
+
+        //Route info      
+        $("#departureTime").text(new Date(route.summary.departureTime).toLocaleTimeString());
+        $("#arrivalTime").text(new Date(route.summary.arrivalTime).toLocaleTimeString());
+        $("#lengthInKm").text((Math.round((route.summary.lengthInMeters / 1000) * 100 / 100)) + ' kilometers');
+        $("#trafficDelayInSeconds").text((Math.round((route.summary.trafficDelayInSeconds / 60) * 100) / 100) + ' minutes');
+        $("#travelTimeInSeconds").text((Math.round((route.summary.travelTimeInSeconds / 60) * 100) / 100) + ' minutes');
+        $("#routeInfo").show();
+
+        var routeCoordinates = [];
+        for (var leg of route.legs) {
+            var legCoordinates = leg.points.map((point) => [point.longitude, point.latitude]);
+            routeCoordinates = routeCoordinates.concat(legCoordinates);
+        }
+
+        var routeLinestring = new atlas.data.LineString(routeCoordinates);
+        map.addLinestrings([new atlas.data.Feature(routeLinestring)], { name: routeLayer });
+
+        // Add Traffic Flow to the Map
+        map.setTraffic({
+            flow: "relative"
+        });
+
+    }).fail(function () {
+        //error
+        console.warn('ERROR(' + err.code + '): ' + err.message);
+    });
+});
+
--- js/azure-maps-search.js
+++ js/azure-maps-search.js
@@ -1,107 +1,116 @@
- //Render map
- var AzureMapsKey = "<your-azure-maps-key>";
- var map = new atlas.Map("map", {
-     "subscription-key": AzureMapsKey
- });
+//Render map
+var AzureMapsKey = "EFpillS0nu5SJSMtF79GWLwjSfH_F78C-udd_4H7ZTg",
+    map = new atlas.Map("map", {
+        "subscription-key": AzureMapsKey
+    });
 
- //Search
- var searchLayer = "search";
- map.addPins([], {
-     name: searchLayer,
-     cluster: false,
-     icon: "pin-red"
- });
+//Search
+var searchLayer = "search";
+map.addPins([], {
+    name: searchLayer,
+    cluster: false,
+    icon: "pin-red"
+});
 
- //HTML 5 geolocation and Azure Maps Search
- function success(pos) {
-     var url = "https://atlas.microsoft.com/search/fuzzy/json?api-version=1.0&query=bank&subscription-key=" + AzureMapsKey + "&lat=" + pos.coords.latitude + "&lon=" + pos.coords.longitude + "&radius=100000";
-     debugger;
-     $.ajax({
-         url: url,
-         type: "GET"
+//HTML 5 geolocation and Azure Maps Search
+var yourLocation;
+function success(pos) {
+    yourLocation = pos;
+    
+    $.ajax({
+        url: 'https://atlas.microsoft.com/search/fuzzy/json',
+        type: "GET",
+        data: {
+            'api-version': '1.0',
+            'subscription-key': AzureMapsKey,
+            'query': 'restaurant',
+            'lat': pos.coords.latitude,
+            'lon': pos.coords.longitude,
+            'radius': '100000'
+        }
 
-     }).done(function (data) {
-         var searchPins = data.results.map((poi) => {
-             var poiPosition = [poi.position.lon, poi.position.lat];
-             return new atlas.data.Feature(new atlas.data.Point(poiPosition), {
-                 name: poi.poi.name,
-                 address: poi.address.streetName + ", " + poi.address.streetNumber,
-                 position: poi.position.lat + ", " + poi.position.lon,
-                 phone: poi.poi.phone,
-                 categories: poi.poi.categories.toString()
-             });
-         });
+    }).done(function (data) {
+        var searchPins = data.results.map((poi) => {
+            var poiPosition = [poi.position.lon, poi.position.lat];
+            return new atlas.data.Feature(new atlas.data.Point(poiPosition), {
+                name: poi.poi.name,
+                address: poi.address.streetName + ", " + poi.address.streetNumber,
+                position: poi.position.lat + ", " + poi.position.lon,
+                phone: poi.poi.phone,
+                categories: poi.poi.categories.toString()
+            });
+        });
 
-         map.addPins(searchPins, {
-             name: searchLayer
-         });
-
-
-         //Get all longitudes
-         var lons = searchPins.map((pin) => { return pin.geometry.coordinates[0] });
-         //Get all latitudes
-         var lats = searchPins.map((pin) => { return pin.geometry.coordinates[1] });
-
-         //the min longitude and latitude
-         var swLon = Math.min.apply(null, lons);
-         var swLat = Math.min.apply(null, lats);
-         //the max longitude and latitude
-         var neLon = Math.max.apply(null, lons);
-         var neLat = Math.max.apply(null, lats);
-
-         //Set the bounds of the map
-         map.setCameraBounds({
-             bounds: [swLon, swLat, neLon, neLat],
-             padding: 50
-         });
-
-     });
-
-     // Add a popup to the map which will display some basic information about a search result on hover over a pin
-     var popup = new atlas.Popup();
-     map.addEventListener("mouseover", searchLayer, (e) => {
-         var popupContentElement = document.createElement("div");
-         // popupContentElement.style.padding = "5px";
-         popupContentElement.className = "poi-content-box";
-
-         var popupNameElement = document.createElement("div");
-         popupNameElement.innerText = e.features[0].properties.name;
-         popupNameElement.className = "poi-title-box";
-         popupContentElement.appendChild(popupNameElement);
-
-         var popupInfoBoxElement = document.createElement("div");
-         popupInfoBoxElement.className = "poi-info-box font-segoeui";
-         popupContentElement.appendChild(popupInfoBoxElement);
+        map.addPins(searchPins, {
+            name: searchLayer
+        });
 
 
-         var popupAddressElement = document.createElement("div");
-         popupAddressElement.innerText = e.features[0].properties.address;
-         popupAddressElement.className = "info location";
-         popupInfoBoxElement.appendChild(popupAddressElement);
+        //Get all longitudes
+        var lons = searchPins.map((pin) => { return pin.geometry.coordinates[0] });
+        //Get all latitudes
+        var lats = searchPins.map((pin) => { return pin.geometry.coordinates[1] });
 
-         var popupPositionElement = document.createElement("div");
-         popupPositionElement.innerText = e.features[0].properties.phone;
-         popupPositionElement.className = "info phone";
-         popupInfoBoxElement.appendChild(popupPositionElement);
+        //the min longitude and latitude
+        var swLon = Math.min.apply(null, lons);
+        var swLat = Math.min.apply(null, lats);
+        //the max longitude and latitude
+        var neLon = Math.max.apply(null, lons);
+        var neLat = Math.max.apply(null, lats);
 
-         var popupCategoriesElement = document.createElement("div");
-         popupCategoriesElement.innerText = e.features[0].properties.categories;
-         popupCategoriesElement.className = "info category";
-         popupInfoBoxElement.appendChild(popupCategoriesElement);
+        //Set the bounds of the map
+        map.setCameraBounds({
+            bounds: [swLon, swLat, neLon, neLat],
+            padding: 50
+        });
 
-         popup.setPopupOptions({
-             position: e.features[0].geometry.coordinates,
-             content: popupContentElement
-         });
+    });
 
-         popup.open(map);
-     });
+    // Add a popup to the map which will display some basic information about a search result on hover over a pin
+    var popup = new atlas.Popup();
+    map.addEventListener("mouseover", searchLayer, (e) => {
+        var popupContentElement = document.createElement("div");
+        // popupContentElement.style.padding = "5px";
+        popupContentElement.className = "poi-content-box";
 
- }
+        var popupNameElement = document.createElement("div");
+        popupNameElement.innerText = e.features[0].properties.name;
+        popupNameElement.className = "poi-title-box";
+        popupContentElement.appendChild(popupNameElement);
 
- function error(err) {
-     console.warn('ERROR(' + err.code + '): ' + err.message);
- }
+        var popupInfoBoxElement = document.createElement("div");
+        popupInfoBoxElement.className = "poi-info-box font-segoeui";
+        popupContentElement.appendChild(popupInfoBoxElement);
 
- //Get your coordinates
- navigator.geolocation.getCurrentPosition(success, error, { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 });
+
+        var popupAddressElement = document.createElement("div");
+        popupAddressElement.innerText = e.features[0].properties.address;
+        popupAddressElement.className = "info location";
+        popupInfoBoxElement.appendChild(popupAddressElement);
+
+        var popupPositionElement = document.createElement("div");
+        popupPositionElement.innerText = e.features[0].properties.phone;
+        popupPositionElement.className = "info phone";
+        popupInfoBoxElement.appendChild(popupPositionElement);
+
+        var popupCategoriesElement = document.createElement("div");
+        popupCategoriesElement.innerText = e.features[0].properties.categories;
+        popupCategoriesElement.className = "info category";
+        popupInfoBoxElement.appendChild(popupCategoriesElement);
+
+        popup.setPopupOptions({
+            position: e.features[0].geometry.coordinates,
+            content: popupContentElement
+        });
+
+        popup.open(map);
+    });
+
+}
+
+function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+}
+
+//Get your coordinates
+navigator.geolocation.getCurrentPosition(success, error, { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 });
